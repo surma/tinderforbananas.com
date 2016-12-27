d24c04353fa2dfb7ab6d0462803c205e5e0d3e15
Surma <surma@surma.link>
2016-12-27 16:48:40 +0100

    Switch to Babili

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const cssnext = require('postcss-cssnext');
-const closure = require('google-closure-compiler').compiler;
+const babel = require('gulp-babel');
 const through = require('through2');
 const htmlMinifier = require('gulp-html-minifier');
 
@@ -11,30 +11,10 @@ gulp.task('build-css', _ =>
     .pipe(gulp.dest('dist'))
 );
 
-function closureCompile(file) {
-  return new Promise((resolve, reject) => {
-    const compiler = new closure({
-      js: file,
-      compilation_level: 'WHITESPACE_ONLY',
-      language_in: 'ECMASCRIPT6_STRICT',
-      language_out: 'ECMASCRIPT6_STRICT'
-    })
-    .run((exitCode, stdOut, stdErr) => {
-      if (exitCode === 0) return resolve(stdOut);
-      reject(stdErr);
-    });
-  });
-}
-
 gulp.task('build-js', _ =>
   gulp.src('app/*.js')
-    .pipe(through.obj((file, enc, cb) => {
-      closureCompile(file.path)
-        .then(output => {
-          file.contents = new Buffer(output);
-          cb(null, file);
-        })
-        .catch(err => console.error(err));
+    .pipe(babel({
+      presets: ['babili']
     }))
     .pipe(gulp.dest('dist'))
 );

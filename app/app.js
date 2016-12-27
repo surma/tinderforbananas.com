e5d2307cf66053aec4f8221a598dcac64b641157
Surma <surma@surma.link>
2016-12-27 14:20:20 +0100

    Some preliminary Safari fixes

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
       name: 'Saba',
       age: 25,
       job: 'Phillippines',
-      images: ['testpic1.jpg', 'testpic2.jpg', 'testpic3.jpg'],
+      images: ['images/testpic1.jpg', 'images/testpic2.jpg', 'images/testpic3.jpg'],
       distance: 25,
       description: 'Lol 😂'
     },
@@ -12,7 +12,7 @@
       name: 'Plantain',
       age: 28,
       job: 'Nicaragua',
-      images: ['testpic2.jpg', 'testpic4.jpg'],
+      images: ['images/testpic2.jpg', 'images/testpic4.jpg'],
       distance: 4,
       description: 'Lorem ipsum dolor sit amet, quo ad cibo viris legimus, simul delicata constituto per cu. Pro an commodo liberavisse, cu mutat sensibus tractatos est, animal similique ei nec. Et est molestie phaedrum, ut eam quot meliore. Usu hendrerit complectitur at, at iriure habemus facilisis sit. An eos probo graece.Propriae contentiones eu ius, pro eu ignota liberavisse disputationi, duo ea docendi consectetuer. Cum posse semper ea, ius invidunt qualisque scriptorem cu, ullum reprehendunt pro eu. Illud erant reformidans usu in. Ad vim quem choro iracundia. Ius in case mnesarchum.Duis signiferumque sed cu. Ut duo error congue intellegebat, fugit nostrud urbanitas ei has. Copiosae dissentias te eam, dicta efficiendi mea ad. Numquam persequeris te sea, ad populo graeci per, et mea aperiam noluisse interesset.Malorum abhorreant pri eu, no vidit quaeque mei, usu in dico meliore philosophia. Causae verterem pri in, te case suavitate nam. In ius ignota sanctus. Propriae repudiandae ad sit, gubergren ullamcorper usu ei. Ne vis fierent mediocritatem. Id nominati maluisset ius, soluta graece lobortis ut his, vocibus copiosae placerat est ad.Duo alia ferri impetus ei, deleniti scriptorem comprehensam ius an. Mea ne labore oblique adolescens. Ne velit albucius salutatus quo, cum iudico eripuit bonorum ad. Stet suscipit sea ad. Nec prompta suscipit mandamus at.'
     },
@@ -20,7 +20,7 @@
       name: 'Banan',
       age: 21,
       job: 'Finnland',
-      images: ['testpic3.jpg'],
+      images: ['images/testpic3.jpg'],
       distance: 9,
       description: 'I like fruits!'
     },
@@ -28,7 +28,7 @@
       name: 'Actually an orange',
       age: 12,
       job: 'Scammer',
-      images: ['testpic4.jpg'],
+      images: ['images/testpic4.jpg'],
       distance: 2455,
       description: 'Follow me on my totally banana-related Instagram 📸'
     }
@@ -191,5 +191,5 @@
     window.addEventListener('resize', adjustNextItem);
     hookupButtons();
   }
-  init();
+  document.addEventListener('DOMContentLoaded', init);
 })();
